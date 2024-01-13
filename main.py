d4b03b81e926368e8fcbe4bb2a79302527acd0da
Oleg <klaus888@yandex.ru>
2024-01-13 23:41:10 +0300

    worked

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 from aiogram import Bot, Dispatcher, executor, types
+import json
+
+with open('db_SokololBot.json', 'r') as f:
+    token = json.loads(f.read())['bot_token']
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+
+dp = Dispatcher(bot=bot)
+
+# Хэндлер ответа
+@dp.message_handler(commands=['start', 'help', '1-10'])
+async def cmd_start(message: types.Message):
+    if message.get_command() == r'/start':
+        await message.reply('Get start!@')
+    elif message.get_command() == r'/help':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ['Menu', 'random', 'Order']
+        keyboard.add(*buttons)
+        await message.answer('Menu:', reply_markup=keyboard)
+        
+        
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
